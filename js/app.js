f5b8f20e1a5de83f18324825e99d7c75201efc16
aomine <xomarxpl07@outlook.es>
2019-03-15 20:44:31 -0500

    v 1.7

--- js/app.js
+++ js/app.js
@@ -17,6 +17,9 @@ sections = [];
 //Load
 window.onload = () => {
 	headerH = header.offsetHeight;
+	main.style.height = (window.innerHeight - headerH)+'px';
+	// main.style.height = document.documentElement.clientHeight;
+
 	section.forEach((e, i) => {
 		sections[e.id] = e.offsetTop - headerH;
 	});
@@ -65,6 +68,7 @@ function removeV(){
 //Resize
 window.onresize = ()=>{
 	headerH = header.offsetHeight;
+	main.style.height = (window.innerHeight - headerH)+'px';
 	section.forEach((e, i) => {
 		sections[e.id] = e.offsetTop - headerH;
 	});
